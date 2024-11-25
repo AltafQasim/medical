d22c1cbf1ee41d53923c4ae2e9f7781b140ae316
Ronak Patel <ronak.buoyancy@gmail.com>
2024-11-25 12:52:14 +0530

    initial commit

--- src/Components/NotFound.jsx
+++ src/Components/NotFound.jsx
@@ -1,11 +1,12 @@
-import { useEffect } from "react"
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
-const NotFound=()=>{
-    const navigate=useNavigate();
-    useEffect(() => {
-        navigate("/");
-    }, []);
-    return <></>;
-}
+const NotFound = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    navigate("/");
+  }, []);
+
+  return <></>;
+};
 export default NotFound;
